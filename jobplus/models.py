dc68f09f96fb3f8ceb552765dc3c022d8a94fb6e
xiao-mianyang <joey.sin@qq.com>
2019-11-23 13:39:16 +0800

    fix #4 update task4

--- jobplus/models.py
+++ jobplus/models.py
@@ -102,4 +102,64 @@ class ProjectExperice(Experience):
     resume = db.relationship('Resume', uselist=False)
 
 
+class Company(Base):
+    __table__ = 'company'
+
+    id = db.Column(db.Integer, primary_key=Ture)
+    name = db.Column(db.String(64),nullable=False,index=True,unique=True)
+    slug = db.Column(db.String(24),nullable=False,index=True,unique=True)
+    logo= db.Column(db.String(64),nullable=False)
+    site = db.Column(db.String(64),nullable=False)
+    contact=db.Column(db.String(24),nullable=False)
+    email = db.Column(db.String(24),nullable=False)
+    location = db.Column(db.String(24),nullable=False)
+    description = db.Column(db.String(100))
+    about = db.Column(db.String(1024))
+    tags = db.Column(db.String(128))
+    stack = db.Column(db.String(128))
+    team_introduction = db.Column(db.String(256))
+    welfares = db.Column(db.String(256))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), ondelete='SET NULL')
+    user = db.relationship('User', uselist=False, backref=db.backref('company', uselist=False))
+
+    def __repr__(self):
+        return '<Company {}>'.format(self.name)
+
+
+class Job(Base):
+    __tablename__ = 'job'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(24))
+    salary_low = db.Column(db.Integer, nullable=False)
+    salary_high = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(24))
+    tags = db.Column(db.String(128))
+    experience_requirement = db.Column(db.String(32))
+    degree_requirement = db.Column(db.String(32))
+    is_fulltime = db.Column(db.Boolean, default=True)
+    is_open = db.Column(db.Boolean, default=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id', ondelete='CASCADE'))
+    company = db.relationship('Company', uselist=False)
+    views_count = db.column(db.Integer, default=0)
+    
+    def __repr__(self):
+        return '<Job {}>'.format(self.name)
+
+
+class Dilivery(Base):
+    __tablename__ = 'delivery'
+
+
+    STATUS_WAITING = 1
+    STATUS_REJECT = 2
+    STATUS_ACCEPT = 3
+
+    id = db.Column(db.Integer, primary_key=True)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id', ondelete='SET NULL'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='SET NULL'))
+    status = db.Column(db.SmallInteger, default=STATUS_WAITING)
+
+    response = db.Column(db.String(256))
+
 
